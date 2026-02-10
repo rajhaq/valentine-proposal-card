5938093418f3d9d1f7686d06adde379b2ee04173
rajhaq <zubaer.haque@gmail.com>
2026-02-10 21:46:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: "/valentine-proposal-card/",
+    plugins: [react()],
     const env = loadEnv(mode, '.', '');
     return {
       server: {
